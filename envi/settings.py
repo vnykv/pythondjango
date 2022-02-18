9f94b08d108da7aab52953bc77ff6d67899b5dff
vnykv <braj22380@gmail.com>
2022-02-19 05:03:40 +0530

    y

--- envi/settings.py
+++ envi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7=cvk%h@fvpmwhnp7^qnh-ufng)ny9+$2ml1$v-&eefai1l52a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
